6b86615a89525ebdf6900a83861ad5e4bef9934d
KatarzynaKurylo <92303444+KatarzynaKurylo@users.noreply.github.com>
2021-10-25 20:12:10 +0200

    Add files via upload

---
+++ cw2/cw2/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cw2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Cena.Content="0"; 
+        }
+        public MainWindow(string text) : this()
+        {
+            Cena.Content = text;
+        }
+        public MainWindow(string text,string poprzednia_wartosc) : this()
+        {
+            int zmienna=Convert.ToInt32(text.ToString()) + Convert.ToInt32(poprzednia_wartosc);
+            Cena.Content=zmienna.ToString();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 okno1 = new Window1();
+            okno1.Show();
+            this.Close();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Window2 okno2 = new Window2(Cena.Content.ToString());
+            okno2.Show();
+            this.Close();
+        }
+    }
+}
---
+++ cw2/cw2/Window2.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cw2
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window2.xaml
+    /// </summary>
+    public partial class Window2 : Window
+    {
+        private string wartosc;
+        private string wyslij;
+        public Window2(string poprzednia)
+        {
+            InitializeComponent();
+            price.Content = "0";
+            wyslij = poprzednia;
+            combo.Items.Add("100");
+            combo.Items.Add("150");
+            combo.Items.Add("300");
+            combo.Items.Add("500");
+        }
+        private void Benzyna_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Benzyna.IsChecked == true)
+            {
+                price.Content = "10000";
+            }
+        }
+        private void Diesel_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Diesel.IsChecked == true)
+            {
+                price.Content="20000";
+            }
+        }
+        private void Hybryda_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Hybryda.IsChecked == true)
+            {
+                price.Content="50000";
+            }
+        }
+        private void Gaz_Checked(object sender, RoutedEventArgs e)
+        {
+            if (Gaz.IsChecked==true)
+            {
+                price.Content="25000";
+            }
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wartosc=price.Content.ToString();
+            MainWindow okno_koncowe=new MainWindow(wartosc,wyslij);
+            okno_koncowe.Show();
+            this.Close();
+        }
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int iVal;
+            if (int.TryParse(combo.Text, out iVal))
+            {
+                int zmienna = Convert.ToInt32(price.Content) + iVal*100;
+                price.Content = zmienna.ToString();
+            }
+        }
+
+    }
+}
